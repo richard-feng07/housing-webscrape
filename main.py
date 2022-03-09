8cc9e088a7c975620910496dd259969d10a2e4c8
richard-feng07 <richardfeng43@gmail.com>
2022-03-08 22:13:58 -0800

    added trulia
    Trying to fix realtor and # of homes.

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def Scrape_Zillow():
             house_type = content.find(class_="list-card-statusText")
             house_address = content.find(class_="list-card-addr")
             house_link = content.find(class_="list-card-link list-card-link-top-margin list-card-img")
-
+            house_realtor = content.find(class_="list-card-extra-info")
             children = house_specs.findChildren()
             house_spec_list = []
             for child in children:
@@ -73,5 +73,42 @@ def Scrape_Zillow():
             print(house_bedroom_num[0], "bedrooms |", house_bathroom_num[0], "bathrooms |", house_sqft[0], "sqft")
             print(house_price.text.strip())
             print(house_link["href"])
+            print(house_realtor.text)
             print()
-Scrape_Zillow()
+
+def Scrape_Trulia():
+    print()
+    req_headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
+    }
+    with requests.Session() as s:
+        # state = str(input("Where state? (Abbreviate)"))
+        # city = str(input("What city?"))
+        URL = 'https://trulia.com/CA/Irvine'
+        page = s.get(URL, headers=req_headers)
+        soup = BeautifulSoup(page.content, "html.parser")
+        house_num = soup.find(class_="Text__TextBase-sc-1cait9d-0-div Text__TextContainerBase-sc-1cait9d-1 dPaHma SearchResultsRemoveBoundary__Container-bx3h4q-1 iFEACH")
+        results = soup.find_all(class_="Box__BoxElement-sc-1f5rw0h-0 bsjsJO PropertyCard__PropertyCardContainer-m1ur0x-4 faCQjz")
+
+        # nums = house_num.findChildren()
+        print(house_num)
+        for content in results:
+            house_price = content.find(class_="Text__TextBase-sc-1cait9d-0-div Text__TextContainerBase-sc-1cait9d-1 keMYfJ")
+            house_sqft = content.find(class_="Text__TextBase-sc-1cait9d-0-div Text__TextContainerBase-sc-1cait9d-1 dZyoXR")
+            house_bdrooms = content.find(class_="Text__TextBase-sc-1cait9d-0-div Text__TextContainerBase-sc-1cait9d-1 bjqKkI")
+            house_bthrooms = content.find("div", attrs={"data-testid":"property-baths"})
+            house_address = content.find("div", attrs={"data-testid":"property-address"})
+            house_link = content.find(class_="Anchor__StyledAnchor-sc-5lya7g-1 gLFHbk")
+            house_realtor = content.find(class_="Text__TextBase-sc-1cait9d-0-div Text__TextContainerBase-sc-1cait9d-1 ccZuZv PropertyCard__CapitalizedText-m1ur0x-1 kehSAX")
+            
+            # print(house_bdrooms.text, house_bthrooms.text, house_sqft.text.strip())
+            # print(house_price.text.strip())
+            # print(house_address['title'])
+            # print("https://trulia.com" + house_link['href'])
+            # print(house_realtor)
+            # print()
+Scrape_Trulia()
